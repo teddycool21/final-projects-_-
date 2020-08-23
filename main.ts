78ec3fa70e0b45d057da322fa7368a5e2bf59e1c
teddycool21 <teddylesmana21@gmail.com>
2020-08-23 11:38:59 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        basic.showString("T")
+        basic.showString("Tsunami is here!")
         music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Forever)
         while (receivedNumber == 1) {
             pins.digitalWritePin(DigitalPin.P1, 0)
@@ -10,7 +10,7 @@ radio.onReceivedNumber(function (receivedNumber) {
         }
     } else {
         if (receivedNumber == 2) {
-            basic.showString("E")
+            basic.showString("Earthquake is here!")
             music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Forever)
             while (receivedNumber == 2) {
                 pins.digitalWritePin(DigitalPin.P2, 0)
@@ -19,7 +19,7 @@ radio.onReceivedNumber(function (receivedNumber) {
                 basic.pause(500)
             }
         } else {
-            basic.showString("A")
+            basic.showString("Tsunami and Earthquake are here!")
             music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
             while (receivedNumber == 3) {
                 pins.digitalWritePin(DigitalPin.P1, 0)
@@ -33,17 +33,20 @@ radio.onReceivedNumber(function (receivedNumber) {
     }
 })
 input.onGesture(Gesture.EightG, function () {
-    radio.sendString("Tsunami and Earthquake are here!")
-    Detector = 3
-    radio.sendNumber(Detector)
+	
 })
-input.onGesture(Gesture.FreeFall, function () {
+input.onButtonPressed(Button.A, function () {
     radio.sendString("Tsunami is here!")
     Detector = 1
     radio.sendNumber(Detector)
 })
-radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
+input.onGesture(Gesture.FreeFall, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("Tsunami and Earthquake are here!")
+    Detector = 3
+    radio.sendNumber(Detector)
 })
 input.onGesture(Gesture.Shake, function () {
     radio.sendString("Earthquake is here!")
