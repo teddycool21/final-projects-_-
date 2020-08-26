8752b88bc1ee7753b90a864759c17148c4f62df4
teddycool21 <teddylesmana21@gmail.com>
2020-08-26 21:23:25 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,61 +3,59 @@ radio.onReceivedNumber(function (receivedNumber) {
         basic.showString("Tsunami is here!")
         while (!(input.buttonIsPressed(Button.AB))) {
             music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
-            pins.digitalWritePin(DigitalPin.P1, 0)
-            basic.pause(200)
             pins.digitalWritePin(DigitalPin.P1, 1)
-            basic.pause(200)
+            basic.pause(500)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            basic.pause(1000)
         }
     } else {
         if (receivedNumber == 2) {
             basic.showString("Earthquake is here!")
             while (!(input.buttonIsPressed(Button.AB))) {
                 music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
-                pins.digitalWritePin(DigitalPin.P2, 0)
-                basic.pause(500)
                 pins.digitalWritePin(DigitalPin.P2, 1)
-                basic.pause(500)
+                basic.pause(1000)
+                pins.digitalWritePin(DigitalPin.P2, 0)
+                basic.pause(2000)
             }
         } else {
-            basic.showString("Tsunami and Earthquake are here!")
-            while (!(input.buttonIsPressed(Button.AB))) {
-                music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
-                pins.digitalWritePin(DigitalPin.P1, 0)
-                pins.digitalWritePin(DigitalPin.P2, 0)
-                basic.pause(1000)
-                pins.digitalWritePin(DigitalPin.P1, 1)
-                pins.digitalWritePin(DigitalPin.P2, 1)
-                basic.pause(1000)
+            if (receivedNumber == 3) {
+                basic.showString("Tsunami and Earthquake are here!")
+                while (!(input.buttonIsPressed(Button.AB))) {
+                    music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
+                    pins.digitalWritePin(DigitalPin.P1, 1)
+                    pins.digitalWritePin(DigitalPin.P2, 1)
+                    basic.pause(500)
+                    pins.digitalWritePin(DigitalPin.P1, 0)
+                    pins.digitalWritePin(DigitalPin.P2, 0)
+                }
             }
         }
     }
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 0)
+    Detector = 0
     basic.clearScreen()
-})
-input.onGesture(Gesture.EightG, function () {
-    radio.sendString("Tsunami and Earthquake are here!")
-    Detector = 3
-    radio.sendNumber(Detector)
 })
 input.onGesture(Gesture.FreeFall, function () {
     radio.sendString("Tsunami is here!")
     Detector = 1
     radio.sendNumber(Detector)
 })
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.LogoUp, function () {
     radio.sendString("Earthquake is here!")
     Detector = 2
+    radio.sendNumber(Detector)
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendString("Tsunami and Earthquake are here!")
+    Detector = 3
     radio.sendNumber(Detector)
 })
 let Detector = 0
 radio.setGroup(100)
 radio.setTransmitPower(1)
 Detector = 0
-pins.onPulsed(DigitalPin.P0, PulseValue.High, function () {
-    devices.raiseAlertTo(MesAlertEvent.RingAlarm)
-})
 basic.forever(function () {
 	
 })
-
